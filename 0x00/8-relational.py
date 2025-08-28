a1487935f7fb1d5a474e4cc0c9cd79ed33c03e42
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-08-28 14:03:37 +0000

    python3

---
+++ 0x00/8-relational.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a module that handles all the relational operators"""
+num1 = int(input("Enter the first number;"))
+num2 = int(input("Enter the second nuumber;"))
+
+print(f"Greater than {num1} > {num2} = {num1>num2}")
+print(f"Greater than or equal to {num1} >= {num2} = {num1>=num2}")
+print(f"Less than or equal to {num1} <= {num2} = {num1<=num2}")
+print(f"Less than {num1} < {num2} = {num1<num2}")
+print(f"Equal to  {num1} == {num2} = {num1==num2}")
+print(f"Not equal to  {num1} != {num2} = {num1!=num2}")
