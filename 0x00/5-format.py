7dd019d9facf2097287a9e1b4940c5692cfb7b55
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-08-28 13:09:34 +0000

    python3 5-format.py

---
+++ 0x00/5-format.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a module that handles string format"""
+age = 13
+name = "Ugochukwu Kamsi"
+formattedstring = "My name is {},and i am {} years old.".format(name, age)
+print(formattedstring)
