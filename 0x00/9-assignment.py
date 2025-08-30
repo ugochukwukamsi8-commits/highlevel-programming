ff40ee679942a38b823a62af7f470cf08edee9f5
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-08-30 14:14:21 +0000

    python3

---
+++ 0x00/9-assignment.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""a module to create a standard assignment"""
+    
+print("Enter the first number:")
+num1 = float(input())
+print("Enter the second number:")
+num2 = float(input())
+x = num1
+print(f"x = num1: x is now {x}")
+x -= num2  
+x = num1 
+print("Cannot perform x /= num2: Division by zero.")
+x = num1 
+num2 != 0
+x %= num2  
+print(f"x %= num2: x is now {x}")
+print("Cannot perform x %= num2: Division by zero.")
+x = num1 
+num2 != 0
+x //= num2  
+print(f"x //= num2: x is now {x}")
+print("Cannot perform x //= num2: Division by zero.")
+x = num1 
+x **= num2 
+print(f"x **= num2: x is now {x}")
