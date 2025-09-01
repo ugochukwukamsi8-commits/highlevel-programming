ee95ed27fa49bacb411672e1c710507355d5d8aa
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-09-01 13:52:38 +0000

    python3

---
+++ 0x01/2-elif.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a module that handles elseif conditions"""
+age = 13
+
+if age == 13:
+    print("you are a teenager")
+elif age >= 18:
+    print("you are an adult")
+else:
+    print("you are a child")
