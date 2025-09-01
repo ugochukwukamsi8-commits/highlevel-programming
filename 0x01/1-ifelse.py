3fa9f0756ebcc713c9fb8ac71b02ac19503a7d39
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-09-01 13:49:07 +0000

    python3

---
+++ 0x01/1-ifelse.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a module that handles if else conditions """
+age = 13
+
+if age  >= 18:
+    print("you are an adult and can vote")
+else:
+    print("you are a child try when you are as old as i am")
