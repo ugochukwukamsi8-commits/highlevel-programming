7cb2102b860acafb78b9e3fadbca213e5cbe2a2a
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-09-04 16:19:40 +0000

    python3

---
+++ 0x01/2-multicondition.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a module that handles conditionals"""
+age = int(input("enter your age"))
+""""""
+if age >= 18:
+    print("you are an adult")
+else:
+    print:("you are a child")
