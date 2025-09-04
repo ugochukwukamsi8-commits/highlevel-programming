f9ccef19560f2c9fed1195e5c57f433ae04f1ed8
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-09-04 11:59:46 +0000

    python3

---
+++ 0x01/1-ifelse.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a module that handles conditional statement"""
+age = int(input("Enter Your age"))
+if age >= 18:
+    print("Youare an adult")
+else:
+    print("You are a child")
