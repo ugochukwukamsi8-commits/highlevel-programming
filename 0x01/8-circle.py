1880a2fdb04280628755e8742e2d314068b119d7
kamsi ugochukwu <ugochukwukamsi8@gmail.com>
2025-09-05 14:23:40 +0000

    python3

---
+++ 0x01/8-circle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a module that implements the area of rectangle"""
+
+def circle(r):
+    return 3.142**r
+
+
+if __name__ == "__main__":
+    r = int(input("enter the value of r²"))
+    print(circle(r)) 
